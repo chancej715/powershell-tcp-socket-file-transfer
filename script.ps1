2ba5802ef5b9d62774f574c90fc14a1385c4ef99
Chance Johnson <chancej715@gmail.com>
2023-07-18 17:54:22 -0500

    Added parameter support and help menus.

--- script.ps1
+++ script.ps1
@@ -1,17 +1,46 @@
-$file="PATH"        # File to transfer
-$ip="IP ADDRESS"    # IP of receiving host
-$port="PORT"        # Port of receiving host
+<#
+.SYNOPSIS
+    Write bytes of a file to a stream.
+.DESCRIPTION
+    This PowerShell script establishes a TCP connection with a designated host and subsequently retrieves the associated network stream. Next, it reads the bytes of a file into a buffer and writes the buffer to the network stream.
+.PARAMETER IP
+    IPv4 or IPv6 address of receiving host.
+.PARAMETER Port
+    Port number of receiving host.
+.PARAMETER File
+    File to transfer.
+.EXAMPLE
+    PS> Transfer-File -IP 10.10.10.11 -Port 4444 -File archive.zip 
+.LINK
+    https://github.com/chancej715/powershell-tcp-socket-file-transfer
+.NOTES
+    Author: Chance Johnson (@chancej715)
+#>
 
-$TcpClient=New-Object System.Net.Sockets.TcpClient($ip, $port)
-$Stream = $TcpClient.GetStream()
-[byte[]]$buffer = New-Object byte[] 1024
-$File = [System.IO.File]::OpenRead($file)
+function Transfer-File
+{
+    param (
+        [parameter(mandatory)][string]$IP,
+        [parameter(mandatory)][ValidateRange(1, 65535)][int]$Port,
+        [parameter(mandatory)][string]$File
+    )
+    
+    # Read file
+    [byte[]]$buffer = New-Object byte[] 1024
+    $FileStream = [System.IO.File]::OpenRead($File)
 
-while ( $bytes = $File.Read($buffer,0,$buffer.count) ) {
-    $Stream.Write($buffer,0,$bytes)
-    Write-Host ($bytes)
+    # Open socket
+    $TcpClient=New-Object System.Net.Sockets.TcpClient($IP, $Port)
+    $Stream = $TcpClient.GetStream()
+
+    # Write file to network stream
+    Write-Host "Writing file to network stream..."
+    while ( $bytes = $FileStream.Read($buffer,0,$buffer.count) ) {
+        $Stream.Write($buffer,0,$bytes)
+    }
+
+    Write-Host "Done."
+    $FileStream.Close()
+    $Stream.Dispose()
+    $TcpClient.Dispose()
 }
-
-$File.Close()
-$Stream.Dispose()
-$TcpClient.Dispose()
